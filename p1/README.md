b6111b43994bbc048d36a0ff37259b30413e42d3
Anya Zinoveva <anya@educationalfilms.net>
2020-02-11 17:18:21 -0800

    Practice examples from lecture; start of p1

---
+++ p1/README.md
@@ -0,0 +1,9 @@
+# Project 1
++ By: Anya Zinoveva
++ Production URL: <http://e15p1.atozez.com>
+
+## Outside resources
+n/a
+
+## Notes for instructor
+n/a
--- practice/index.php
+++ practice/index.php
@@ -1,11 +1,49 @@
-<!doctype html>
-<html lang='en'>
-<head>
-	<title>Practice Application</title>
-	<meta charset='utf-8'>
-</head>
-<body>
-	<h1>Practice Application</h1>
-	<img src='images/hes-logo.png'>
-</body>
-</html>
+<?php
+
+# Define 4 different variables, which will
+# each represent how much a given coin is worth
+$penny_value = .01;
+$nickel_value = .05;
+$dime_value = .10;
+$quarter_value = .25;
+
+# Define 4 more variables, which will each
+# represent how many of each coin is in the bank
+$pennies = 100;
+$nickels = 25;
+$dimes = 100;
+$quarters = 34;
+
+# Add up how much money is in the piggy bank
+$total = ($pennies * $penny_value) + ($nickels * $nickel_value) + ($dimes * $dime_value) + ($quarters * $quarter_value);
+# BEFORE
+$phrases = [
+    'hola',
+    'adios',
+    'hasta luego',
+    'por favor',
+    'de nada',
+];
+
+
+var_dump($phrases);
+# AFTER
+foreach ($phrases as $value) {
+    $value = strtoupper($value);
+    //echo $value;
+    var_dump($value);
+}
+
+# For the purpose of these examples, we can "hard-code" $age to some value
+$age = 14;
+$category;
+
+if ($age > 18) {
+    $category = 'adult';
+}
+
+# Use var_dump to test the results
+var_dump($category);
+
+
+require 'index-view.php';
